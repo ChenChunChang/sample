559695b19ef93a170442fa81a5582664a12edbda
vagrant <vagrant@homestead>
2018-06-26 13:32:28 +0000

    5

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     protected $table = 'users';
-    protected $fillable = ['name', 'email', 'password'];
-    protected $hidden = ['password', 'remember_token'];
-
     use Notifiable;
 
     /**
